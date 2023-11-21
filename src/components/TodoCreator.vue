6a5cd1de69b07fdb5cedf73a6e0df99f9bf37e94
rzayev558 <rzayev558@gmail.com>
2023-11-21 16:03:23 +0100

    create todo

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref, defineEmits } from 'vue'
+
+const emit = defineEmits(['create-todo'])
+type todoType = {
+  todo: string
+}
+const todo = ref<todoType>({ todo: '' })
+const allTodos = ref<todoType[]>([])
+
+const CreateTodo = () => {
+  emit('create-todo', todo.value)
+}
+</script>
+
+<template>
+  <div class="input-wrap">
+    <input type="text" v-model="todo.todo" />
+    <button @click="CreateTodo()">Create</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+  &focus-within {
+    box-shadow:
+      0 -4px 6px -1px rgb(0, 0, 0 / 0.1),
+      0 -2px 4px -2px rgb(0, 0, 0 / 0.1);
+  }
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border:none &:focus {
+      outline: none;
+    }
+  }
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+  .ul-element {
+    margin-top: 30px;
+  }
+}
+</style>
---
+++ src/views/TodosView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import TodoCreator from '@/components/TodoCreator.vue'
+import { ref } from 'vue'
+
+const todoList = ref<todoType[]>([])
+type todoType = {
+  todo: string
+}
+const createTodo = (todo: todoType) => {
+  todoList.value.push({ todo: todo.todo })
+}
+</script>
+
+<template>
+  <main>
+    <h1>Create todo</h1>
+    <TodoCreator @create-todo="createTodo" />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
